5e1c6fb8be2b3a83f1bc03d02e30f4dc24a0ef4a
KrishCodesw <krishjain.w@gmail.com>
2026-03-17 12:15:16 +0530

    added the sol wallet

---
+++ components/SolanaWallet.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import nacl from "tweetnacl";
+
+export function SolanaWallet({ mnemonic }: any) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [publicKeys, setPublicKeys] = useState<PublicKey[]>([]);
+
+  return (
+    <div>
+      <button
+        onClick={function () {
+          const seed = mnemonicToSeedSync(mnemonic);
+          const path = `m/44'/501'/${currentIndex}'/0'`;
+          const derivedSeed = derivePath(path, seed.toString()).key;
+          const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+          const keypair = Keypair.fromSecretKey(secret);
+          setCurrentIndex(currentIndex + 1);
+          setPublicKeys([...publicKeys, keypair.publicKey]);
+        }}
+      >
+        Add wallet
+      </button>
+      {publicKeys.map((p) => (
+        <div>{p.toBase58()}</div>
+      ))}
+    </div>
+  );
+}
